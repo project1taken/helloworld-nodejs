08b9a1f3bd48178e69892644b27ba7ffe27ffe43
project1taken <project1@contractor.net>
2017-11-21 21:48:14 -0500

    update

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.static(__dirname + '/public'))
 
 function get_trustyou(trust_you_id, callback) {
     var options = {
-        uri : 'http://ec2-107-20-66-27.compute-1.amazonaws.com:5005/'+trust_you_id,
+        uri : 'http://ec2-107-20-66-27.compute-1.amazonaws.com:5000/'+trust_you_id,
         method : 'GET'
     };
     var res = '';
